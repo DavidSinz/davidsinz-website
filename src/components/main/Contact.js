87dfeb24799e8373069efde1fcce4b040f9d1824
DavidSinz <davidsinz@gmx.net>
2022-03-02 23:01:52 +0100

    adding text to contact section

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -1,5 +1,11 @@
 const Contact = () => {
-  return <>Contact</>;
+  return (
+    <div>
+      <h2>Get in Touch</h2>
+      <p>Do you have a question or just want to say hello, feel free to do so.</p>
+      <button>Say Hello</button>
+    </div>
+  );
 };
 
 export default Contact;
