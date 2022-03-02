22f17f9ebe6fb4803c0918b146ace6f30f5cf626
DavidSinz <davidsinz@gmx.net>
2022-03-02 22:48:28 +0100

    creating a card component

--- src/components/main/Experiences.js
+++ src/components/main/Experiences.js
@@ -1,9 +1,13 @@
+import Card from "../ui/Card";
+
 const Experience = () => {
   return (
     <div>
       <h2>Projects I've built</h2>
       <div>
-        
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
