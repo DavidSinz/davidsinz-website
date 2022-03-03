5b66a78b9c3f0c022821035f04a0448d9247d00f
DavidSinz <davidsinz@gmx.net>
2022-03-03 02:57:00 +0100

    nav bar styling

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,13 +1,11 @@
 const Navbar = () => {
   return (
-    <>
-      <div>
-        <div id="logo"></div>
-        <a href="#">About</a>
-        <a href="#">Experience</a>
-        <a href="#">Contact</a>
-      </div>
-    </>
+    <nav className="nav-bar">
+      <div id="logo"></div>
+      <a href="#">About</a>
+      <a href="#">Experience</a>
+      <a href="#">Contact</a>
+    </nav>
   );
 };
 
